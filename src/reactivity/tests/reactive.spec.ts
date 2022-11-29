1326471653839254fdae61d5f50d56fe9c8a62d2
fengyuqing2012 <fengyuqing2012@dingtalk.com>
2022-11-29 20:12:15 +0800

    reactive和readonly支持嵌套

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -9,4 +9,15 @@ describe('reactive', () => {
     expect(isReactive(observed)).toBe(true);
     expect(isReactive(original)).toBe(false);
   });
+
+  it('nested obj', () => {
+    const original = {
+      nested: { foo: 1 },
+      array: [{ name: 'breezefeng' }]
+    };
+    const observed = reactive(original);
+    expect(isReactive(observed.nested)).toBe(true);
+    expect(isReactive(observed.array)).toBe(true);
+    expect(isReactive(observed.array[0])).toBe(true);
+  });
 });
