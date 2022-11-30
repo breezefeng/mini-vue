9741f76867471bded292f3920b614f4c6bd2c4ce
fengyuqing2012 <fengyuqing2012@dingtalk.com>
2022-11-30 16:01:30 +0800

    实现ref功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -59,13 +59,17 @@ export function track(target: any, key: any) {
     depsMap.set(key, dep);
   }
 
+  trackEffects(dep);
+}
+
+export function trackEffects(dep) {
   if (dep.has(activeEffect)) return;
 
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 }
 
-function isTracking() {
+export function isTracking() {
   return shouldTrack && activeEffect !== undefined;
 }
 
@@ -73,6 +77,10 @@ export function trigger(target: any, key: any) {
   const depsMap = targetMap.get(target);
   const dep = depsMap.get(key);
 
+  triggerEffects(dep);
+}
+
+export function triggerEffects(dep) {
   for (const effect of dep) {
     if (effect.scheduler) {
       effect.scheduler();
